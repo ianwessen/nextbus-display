b2462d9d77b85c7d2966aa8748fb9fab5549b45f
Ian Wessen <ianwessen@gmail.com>
2020-02-05 21:34:39 -0800

    cleanup file

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,7 +1,5 @@
 const CONSTANTS = {
   API_URL: `http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&stopId=15385&routeTag=5`,
-
-  ROUTE_DIRECTION: 'Inbound to Temporary Transbay Terminal',
 }
 
 export default CONSTANTS
